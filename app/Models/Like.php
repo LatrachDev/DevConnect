247265301a63a6e82e17e79a35ed867c5c3395f9
LatrachDev <m.latrach.youcode@gmail.com>
2025-03-03 11:17:36 +0000

    DEV-13 implement the like using the livewire

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'post_id'
+    ];
+    
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
